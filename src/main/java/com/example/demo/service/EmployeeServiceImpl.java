08fb63e5c9d42dc18a766ffe66717582619f8f62
thejashree170520 <05theju@gmail.com>
2021-11-02 17:32:13 +0530

    final commit

--- src/main/java/com/example/demo/service/EmployeeServiceImpl.java
+++ src/main/java/com/example/demo/service/EmployeeServiceImpl.java
@@ -3,11 +3,12 @@ package com.example.demo.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.example.demo.entity.Employee;
 import com.example.demo.exception.EmployeeNotFoundException;
 import com.example.demo.repository.EmployeeRepository;
-
+@Service
 public class EmployeeServiceImpl implements EmployeeService {
 	@Autowired
     private EmployeeRepository employeeRepository;
